6f0c56944560c69992c10f8e9fd8733c8f9d91b7
wincubate <jgh@wincubate.net>
2021-02-12 17:14:42 +0100

    Added Extras

---
+++ Extras/Master-Detail-Subdetail/Data/Module.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Wincubate.DataBindingCollectionsExamples.Data
+{
+    public class Module
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Slide> Slides { get => _slides; }
+        private readonly ObservableCollection<Slide> _slides;
+
+        public Module()
+        {
+            #region Set random slide numbers
+
+            int j = 1;
+            _slides = new ObservableCollection<Slide>();
+            for (int i = 0; i < 5; i++)
+            {
+                _slides.Add(new Slide());
+            }
+
+            
+            #endregion
+        }
+    }
+}
---
+++ Extras/Master-Detail-Subdetail/Data/Slide.cs
@@ -0,0 +1,14 @@
+﻿namespace Wincubate.DataBindingCollectionsExamples.Data
+{
+    public class Slide
+    {
+        private static int Next; // <-- To make sure that all slide numbers are unique
+        
+        public string Number { get; set; }
+
+        public Slide()
+        {
+            Number =  $"Slide {++Next}";
+        }
+    }
+}
